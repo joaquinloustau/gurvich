3e9dc214a80d47893cb9db935376604a2449a580
Joaquin Loustau <joaquinloustau@gmail.com>
2016-02-09 20:24:10 -0800

    Finished setting up submission of form to firebase

--- db-manager/firebase-admin.js
+++ db-manager/firebase-admin.js
@@ -1,10 +1,59 @@
-var postRef = new Firebase('https://popping-heat-3599.firebaseio.com/obras');
 
-post1.put("author", "gracehop");
-post1.put("title", "Announcing COBOL, a New Programming Language");
-postRef.push().setValue(post1);
+var url ="https://popping-heat-3599.firebaseio.com/obras";
 
-post2.put("author", "alanisawesome");
-post2.put("title", "The Turing Machine");
-postRef.push().setValue(post2);
+function submitToFirebase(evt) {
+
+	url ="https://popping-heat-3599.firebaseio.com/obras"
+
+	var collection = $('#collection').val();
+	var title = $('#title').val();
+	var author = $('#author').val();
+	var date = $('#date').val();
+	var technique = $('#technique').val();
+	var medium = $('#medium').val();
+	var width = $('#width').val();
+	var height = $('#height').val();
+	var depth = $('#depth').val();
+	var onDisplay = $('#demo-copy').is(':checked');
+	var location = $('#location').val();
+	var foor = $('#floor').val();
+	var tags = $('#myTags').tagit('assignedTags');
+	var notes = $('#notes').val();
+	var photo = $('#photo').text();
+
+	if (collection) {
+		url += '/' + $('#collection').val().toLowerCase();
+	}
+
+	console.log(url);
+
+	var firebaseRef = new Firebase(url);
+	var postRef = firebaseRef.push();
+
+	postRef.set({
+		collection: collection,
+		title: title, 
+		author: author, 
+		date: date,
+		width: width,
+		height: height,
+		depth: depth, 
+		display: onDisplay,
+		location: location,
+		floor: floor, 
+		tags: tags, 
+		notes: notes
+		// Photo: photo,
+	});
+	evt.preventDefault();
+
+}
+
+window.onload = function () {
+
+	var submit = document.getElementById('submit');
+	submit.onclick = submitToFirebase;
+
+}
+
 
